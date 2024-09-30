4b948e1c745f4447d01a2b97963268f31f47fbbf
Nicholas <nickndolo95@gmail.com>
2024-09-30 07:04:57 +0300

    refactor code

--- src/com/balancedString/Expression.java
+++ src/com/balancedString/Expression.java
@@ -6,20 +6,30 @@ public class Expression {
 
     public boolean isBalanced(String input){
         Stack<Character> stack = new Stack<>();
-        for(char ch : input.toCharArray()){;
-            if(ch == '(' || ch == '<' || ch == '[' || ch == '{')
+        for(char ch : input.toCharArray()){
+            if(isLeftBracket(ch))
                 stack.push(ch);
-            if(ch == ')' || ch == '>' || ch == ']' || ch == '}') {
+
+            if(isRightBracket(ch)) {
                 if(stack.isEmpty()) return false;
                 var top = stack.pop();
-                if(
-                                (ch == ')' && top != '(') ||
-                                (ch == '>' && top != '<') ||
-                                (ch == ']' && top != '[') ||
-                                (ch == '}' && top != '{')
-                ) return false;
+                if(bracketsMatch(top, ch)) return false;
             }
         }
         return stack.empty();
+    }
+
+    private boolean isLeftBracket(char ch){
+        return ch == '(' || ch == '<' || ch == '[' || ch == '{';
+    }
+    private boolean isRightBracket(char ch){
+        return ch == ')' || ch == '>' || ch == ']' || ch == '}';
+    }
+
+    private boolean bracketsMatch(char left, char right){
+             return (right == ')' && left != '(') ||
+                (right == '>' && left != '<') ||
+                (right == ']' && left != '[') ||
+                (right == '}' && left != '{');
     }
 }

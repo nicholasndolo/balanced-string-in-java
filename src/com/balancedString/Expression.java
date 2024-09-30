39b4bbd5f05c36d972802c5c400b9b1482b32fb0
Nicholas <nickndolo95@gmail.com>
2024-09-30 11:40:28 +0300

    refactor code

--- src/com/balancedString/Expression.java
+++ src/com/balancedString/Expression.java
@@ -1,8 +1,13 @@
 package com.balancedString;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class Expression {
+
+    private final List<Character> leftBrackets = Arrays.asList('(', '<', '[', '{');
+    private final List<Character> rightBrackets = Arrays.asList(')', '>', ']', '}');
 
     public boolean isBalanced(String input){
         Stack<Character> stack = new Stack<>();
@@ -12,24 +17,22 @@ public class Expression {
 
             if(isRightBracket(ch)) {
                 if(stack.isEmpty()) return false;
+
                 var top = stack.pop();
-                if(bracketsMatch(top, ch)) return false;
+                if(!bracketsMatch(top, ch)) return false;
             }
         }
         return stack.empty();
     }
 
     private boolean isLeftBracket(char ch){
-        return ch == '(' || ch == '<' || ch == '[' || ch == '{';
+        return leftBrackets.contains(ch);
     }
     private boolean isRightBracket(char ch){
-        return ch == ')' || ch == '>' || ch == ']' || ch == '}';
+        return rightBrackets.contains(ch);
     }
 
     private boolean bracketsMatch(char left, char right){
-             return (right == ')' && left != '(') ||
-                (right == '>' && left != '<') ||
-                (right == ']' && left != '[') ||
-                (right == '}' && left != '{');
+       return leftBrackets.indexOf(left) == rightBrackets.indexOf(right);
     }
 }
